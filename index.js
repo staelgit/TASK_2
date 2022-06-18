2c596723d2fcb1a0a40f2c3efae051cb4caf4acd
styura3000 <styura3000@gmail.com>
2022-06-18 23:18:30 +0300

    v2

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import './index.css';
 
 const isCookieConsent = localStorage.getItem('isCookieConsent');
-const cookieConsentHTML = document.querySelector('.cookie-consent');
-const cookieConsentButtonHTML =
-  cookieConsentHTML.querySelector('.cookie-consent__button');
 
 if (isCookieConsent !== 'true') {
+  const cookieConsentHTML = document.querySelector('.cookie-consent');
+  const cookieConsentButtonHTML = cookieConsentHTML.querySelector(
+    '.cookie-consent__button'
+  );
+  cookieConsentHTML.classList.remove('hide');
   cookieConsentButtonHTML.addEventListener('click', () => {
     localStorage.setItem('isCookieConsent', 'true');
     cookieConsentHTML.classList.add('hide');
   });
-} else {
-  cookieConsentHTML.classList.add('hide');
 }
